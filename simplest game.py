995d24613755559a10b97681228dabf8e4ccfa15
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-05-25 16:23:19 +0530

    Add files via upload

---
+++ simplest game.py
@@ -0,0 +1,6 @@
+#a small game to make a fool out of you..lol Ish is doing this for a long time bro
+import random
+from words import word_list
+z=random.randint(0,len(word_list))
+y=input("ENTER YOUR NAME\n")
+print(y+"looks like",word_list[z])
